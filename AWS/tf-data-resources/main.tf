5241104483b3ad95f97099da9409b899bdbb626b
subhabrata2468 <panda.subhabrata2003@gmail.com>
2024-12-04 23:54:39 +0530

    data-resource

---
+++ AWS/tf-data-resources/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.75.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+data "aws_ami" "name" {
+    most_recent = true
+    owners = [ "amazon" ]
+  
+}
+
+output "aws_ami" { 
+  value = data.aws_ami.name.id
+
+}
+
+resource "aws_instance" "myec2" {
+    ami = data.aws_ami.name.id
+    instance_type = "t2.nano"
+
+    tags = {
+        Name = "Myec2"
+    }
+}  
